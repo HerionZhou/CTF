74482208cc03749bdf379ad78bf50376cd98c684
Herion <996632222@qq.com>
2023-10-12 14:49:34 +0800

    笔记更新

---
+++ Tools/Misc/steghide密码爆破脚本/bruteStegHide.sh
@@ -0,0 +1,10 @@
+#bruteStegHide.sh 
+#!/bin/bash
+
+for line in `cat $2`;do
+    steghide extract -sf $1 -p $line > /dev/null 2>&1
+    if [[ $? -eq 0 ]];then
+        echo 'password is: '$line
+        exit
+    fi  
+done 
---
+++ Tools/Misc/steghide密码爆破脚本/readme.txt
@@ -0,0 +1,6 @@
+steghide软件隐藏文件提取：
+kali下
+steghide extract -sf [文件名]
+
+密码爆破脚本使用：
+./bruteStegHide.sh [文件名] [字典名]
